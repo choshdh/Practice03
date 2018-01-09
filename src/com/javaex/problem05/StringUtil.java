d9289abdd77d0c3fe42848854f741755612a2849
bit-user <bit-user@DESKTOP-C1QR4BV>
2018-01-09 16:37:52 +0900

    5번완료

--- src/com/javaex/problem05/StringUtil.java
+++ src/com/javaex/problem05/StringUtil.java
@@ -2,10 +2,14 @@ package com.javaex.problem05;
 
 public class StringUtil {
     
-    public static String concatString(){
+    public static String concatString(String[] strArray){
        
+    	String total = "";
         //메소드 내용작성
-        
+        for(String s : strArray) {
+        	total +=s;
+        }
+        return total;
     }
 
 }
