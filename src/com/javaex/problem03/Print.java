65d4d0a09497b68336cd72f79990e0f8c3158c19
bit-user <bit-user@DESKTOP-C1QR4BV>
2018-01-09 16:20:35 +0900

    3번완료

--- src/com/javaex/problem03/Print.java
+++ src/com/javaex/problem03/Print.java
@@ -6,6 +6,21 @@ public class Print {
         System.out.println(val);
     }
 
+	public void printer(boolean b) {
+		// TODO Auto-generated method stub
+		System.out.println(b);
+	}
+
+	public void printer(double d) {
+		// TODO Auto-generated method stub
+		System.out.println(d);
+	}
+
+	public void printer(String string) {
+		// TODO Auto-generated method stub
+		System.out.println(string);
+	}
+
     //메소드  작성
     
 }
