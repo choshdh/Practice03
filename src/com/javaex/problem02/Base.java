bf2b0681816a29bb3687cc4e008fa9936f25d0d3
bit-user <bit-user@DESKTOP-C1QR4BV>
2018-01-09 16:20:14 +0900

    2번 완료

--- src/com/javaex/problem02/Base.java
+++ src/com/javaex/problem02/Base.java
@@ -5,6 +5,13 @@ public class Base {
     public void service(String state) {
         
         //코드작성
+    	if(state.equals("낮")) {
+    		day();
+    	}else if(state.equals("밤")) {
+    		night();
+    	}else if(state.equals("오후")) {
+    		afternoon();
+    	}
     }
 
     public void day() {
@@ -12,7 +19,7 @@ public class Base {
     }
 
     public void night() {
-        //
+        System.out.println("night");
     }
     
     public void afternoon(){
