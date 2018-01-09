cf36d6cf72a98846317d9e682eb57581c7b6fef1
bit-user <bit-user@DESKTOP-C1QR4BV>
2018-01-09 16:34:55 +0900

    4번

--- src/com/javaex/problem04/CConverterApp.java
+++ src/com/javaex/problem04/CConverterApp.java
@@ -5,17 +5,17 @@ public class CConverterApp {
 
     public static void main(String[] args) {
 
-        double dollar;
-        double won;
+        double dollar = 100;
+        double won = 1000000;
         
         CConverter.setRate(1118.70);
         
         
         //백만원을 달러로 출력
-
+        System.out.println("백만원은 : " + CConverter.toDoller(won) + " 달러 입니다.");
         
         //100달려를 원으로 출력
-        
+        System.out.println("백달러는 : " + CConverter.toKWR(dollar) + " 입니다.");
         
     }
 
