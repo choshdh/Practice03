1522ed31e759fc7b5361990bff888952be761f7f
bit-user <bit-user@DESKTOP-C1QR4BV>
2018-01-10 10:36:54 +0900

    주석 추가

--- src/com/javaex/problem06/FriendApp.java
+++ src/com/javaex/problem06/FriendApp.java
@@ -15,7 +15,7 @@ public class FriendApp {
         // 친구정보 입력받기
         for(int i=0; i<3; i++) {
         	String friendInfo = sc.nextLine();
-        	// 입력받은 친구정보를 공백으로 분리하여 배열로 생성하여 friend 에 대입
+        	// 입력받은 친구정보를 공백을 구분자로 사용 분리하여 배열로 생성하여 friend 에 대입
         	String[] friend = friendInfo.split(" ");
         	// Friend 객체 생성하여 데이터 넣기
         	Friend f = new Friend();
